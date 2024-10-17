ee26d6cbf0d8f5c0ea2420fd18ee7a8a98b0b24d
Sumit Mahor <144623513+sumitmahor13@users.noreply.github.com>
2024-10-17 16:06:32 +0530

    Create Bubble_Short.java

--- Bubble_Short.java
+++ Bubble_Short.java
@@ -1,1 +1,25 @@
+class Practice {
 
+    public static void bubbleSort(int array[]){
+       for(int i=0; i<=array.length-2; i++){                  //i shows turns 
+        for(int j=0; j<=array.length-2-i; j++){
+            if(array[j] > array[j+1]){
+                int temp; 
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+       }
+    
+        for(int i=0; i<=array.length-1; i++){
+            System.out.print(array[i]+ " ");
+        }
+    }
+
+    public static void main(String args[]){
+        int array[] = {2,5,7,1,6,-3,-5,4,8};
+
+        bubbleSort(array);
+    }
+}
